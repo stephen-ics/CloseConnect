65ed02b56361bcea797d9fbb1ec7b2b0af7d8030
Stephen Ni <stephenni1234@gmail.com>
2022-06-18 23:13:19 -0400

    commit4

--- src/pages/chat-components/chat-ui.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ChatUI = () => {
-  return (
-    <div>ch</div>
-  )
-}
-
-export default ChatUI
--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,9 +1,75 @@
 import React from 'react'
+import "./contact-us.css";
+import discord from "./icons/discord.png";
+import instagram from "./icons/instagram.webp";
+import gmail from "./icons/gmail.webp";
 
 const ContactUs = () => {
   return (
-    <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "90vh"}}>
-        <h1>Contact Us</h1>
+    <div className="contacts">
+        <header><h1>Contacts</h1></header>
+        <div className="container">
+            <div className="profiles">
+                <h1>Stephen Ni</h1>
+                <img className="pfp"></img>
+                <div className="icons">
+                    <a>
+                        <img class="icon" src={discord}></img>
+                    </a>
+                    <a>
+                        <img class="icon" src={instagram}></img>
+                    </a>
+                    <a>
+                        <img class="icon" src={gmail}></img>
+                    </a>
+                </div>
+            </div>
+            <div className="profiles">
+                <h1>Sophie Yang</h1>
+                <img className="pfp"></img>
+                <div className="icons">
+                    <a>
+                        <img class="icon" src={discord}></img>
+                    </a>
+                    <a>
+                        <img class="icon" src={instagram}></img>
+                    </a>
+                    <a>
+                        <img class="icon" src={gmail}></img>
+                    </a>
+                </div>
+            </div>
+            <div className="profiles">
+                <h1>Aditiya Ajay</h1>
+                <img className="pfp"></img>
+                <div className="icons">
+                    <a>
+                        <img class="icon" src={discord}></img>
+                    </a>
+                    <a>
+                        <img class="icon" src={instagram}></img>
+                    </a>
+                    <a>
+                        <img class="icon" src={gmail}></img>
+                    </a>
+                </div>
+            </div>
+            <div className="profiles">
+                <h1>Aaron Dias</h1>
+                <img className="pfp"></img>
+                <div className="icons">
+                    <a>
+                        <img class="icon" src={discord}></img>
+                    </a>
+                    <a>
+                        <img class="icon" src={instagram}></img>
+                    </a>
+                    <a>
+                        <img class="icon" src={gmail}></img>
+                    </a>
+                </div>
+            </div>
+        </div>  
     </div>
   )
 }
